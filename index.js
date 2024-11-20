4ad0b42058510f29f9bbc0d607248714e6720280
Subham5401 <mishrasubham21081998@gmail.com>
2024-11-20 16:53:43 +0530

    installed required dependencies and added index.js(main file)

---
+++ index.js
@@ -0,0 +1,50 @@
+const excelJs = require('exceljs');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'outlook',
+  auth: {
+    user: '',
+    pass: '',
+  },
+});
+
+const emailSign = `
+    Best regards,
+    Subham Mishra
+`;
+
+const sendEmailsFromExcel = async () => {
+  const workbook = new excelJs.Workbook();
+
+  await workbook.xlsx.readFile('');
+
+  const worksheet = workbook.getWorksheet(1);
+
+  worksheet.eachRow((row, rowNumber) => {
+    if (rowNumber > 1) {
+      const email = row.getCell(1).value;
+      const subject = row.getCell(2).value;
+      const body = row.getCell(3).value;
+
+      const bodyWithSign = `${body}\n\n${emailSign}`;
+
+      const mailOptions = {
+        from: '',
+        to: email,
+        subject: subject,
+        text: bodyWithSign,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(`Error sending email to ${email}: `, error);
+        } else {
+          console.log(`Email sent to ${email}: ${info.response}`);
+        }
+      });
+    }
+  });
+};
+
+sendEmailsFromExcel();
